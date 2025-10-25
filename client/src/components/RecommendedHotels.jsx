a028bac5ef04f49e0832cc3750fec5a8be49b653
Veterok454 <releonora60@gmail.com>
2025-10-25 15:13:01 +0300

    completed

--- client/src/components/RecommendedHotels.jsx
+++ client/src/components/RecommendedHotels.jsx
@@ -32,7 +32,7 @@ const RecommendedHotels = () => {
   }
 
   return (
-    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20 pb-6'>
       <Title
         title={
           searchedCities.length > 0
